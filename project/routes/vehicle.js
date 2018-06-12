47afcebf8623e3220336217375306eb0efa90f4a
saurabhmarne9595 <saurabhmarne9595@gmail.com>
2018-06-12 16:57:06 +0530

    adding whole project

---
+++ project/routes/vehicle.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var obj={
+    "Name":"Tesla",
+    "Model":"S",
+    "Number":"MH-12 SA-9500",
+    "Temp":27,
+    "Humidity":94
+}
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    
+  res.send(obj);
+});
+
+router.post('/',function(req,res){
+    console.log("in post");
+    console.log(JSON.stringify(req.body.number));
+    
+    if(req.body.number){
+       
+        res.json(obj)
+
+    }
+    if(!req.body.number){
+        
+        res.json({
+            success:false,
+            msg:"send vehicle Number"
+        })
+    }
+})
+
+module.exports = router;
